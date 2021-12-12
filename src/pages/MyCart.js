86f77f5d4c59194defc85ab0c476705ab017ce67
wonaval <wonaval@gmail.com>
2021-12-12 17:28:59 -0500

    Added ternary for navigation

--- src/pages/MyCart.js
+++ src/pages/MyCart.js
@@ -3,39 +3,55 @@ import { UserContext } from '../context/UserContext'
 import axios from 'axios'
 import env from 'react-dotenv'
 
-const MyCart = () => {
-    const { userState, cartState } = useContext(UserContext)
-    const [ cart, setCart ] = cartState
+const MyCart = (props) => {
+    const { userState, cartState, productState } = useContext(UserContext)
+    const [ cart, setCart ] = useState([])
+    const [ products, setProducts ] = productState
+    const [ cartInfo, setCartInfo ] = useState([])
 
+    // GET cart and sets to userState context if username is in localstorage
+    const getCart = async () => {
+        const userId = localStorage.getItem('userId')
+        try {
+        if(userId) {
+            // GET cart from backend
+            const userId = localStorage.getItem('userId')
+            const cartResponse = await axios.get(`${env.BACKEND_URL}/cart`,{
+                headers: { Authorization: userId }
+            })
+            const cartList = cartResponse.data.items;
+            setCart(cartList)
+            const cross = cartList.map((item)=>{
+                return products.find((product)=> product.id === item.itemId)
+            })
+            setCartInfo(cross)
+        }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     useEffect(()=>{
-        // loadProducts()
+        getCart();
     }, [])
-
-    // Cross Reference cartState against All Products Returns Product Info
-    // const loadProducts = async () => {
-    //     try {
-
-    //         console.log(productList)
-    //         const cartList = await cart.map((item)=>{
-    //             return productList.find((product)=> product.id === item.itemId)
-    //         })
-    //         await setCartInfo(cartList)
-
-    //     } catch (error) {
-    //         console.log(error.message)
-    //     }
-    // }
 
     return (
         <div>
             Cart Page
             <div>
-                {/* {cartInfo.map((item, i)=>{
+                { cartInfo.map((item, i)=>{
                     return (
-                        <div key={i}>{item.name}</div>
+                    <div>{cart[i].checkedOut}</div>
+                        // { cart[i].checkedOut ?
+                        // null
+                        // :
+                        // <div key={i}>
+                        //     <span>{item.name}</span>
+                        //     <span><button>Remove</button></span>
+                        // </div>
+                        // }
                     )
-                })} */}
+                }) }
             </div>
         </div>
     )
