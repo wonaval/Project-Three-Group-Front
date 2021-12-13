5e44dfb06095a9931031abdee7f01a109a851c36
typeluis <typeluis@gmail.com>
2021-12-13 14:31:45 -0500

    check out

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
+import env from 'react-dotenv'
 
 const CheckOut = () => {
   // useState
@@ -8,13 +9,16 @@ const CheckOut = () => {
 
   const submitForm = async (e) => {
     try {
-        const userId = localStorage.getItem('userId')
         e.preventDefault()
-        await axios.put('/cart', {
-        headers : { Authorization : userId }
+        const userId = localStorage.getItem('userId')
+        const response = await axios.put(`${env.BACKEND_URL}/cart`, {
+          headers: {
+            Authorization: localStorage.getItem('userId')
+          }
         })
-        console.log('Checked out!')
+        console.log('Checked out!', response)
     } catch (error) {
+      console.log(env.BACKEND_URL)
         console.log(error.message)
     }
   }
@@ -22,11 +26,11 @@ const CheckOut = () => {
 
   return (
     <div>
-      <form onSubmit={submitForm}>
+      <form onSubmit={(e)=>{submitForm(e)}}>
         <label htmlFor='address'>Address:</label>
-        <input type='text' placeholder='Enter address...' onChange={(e)=>{setAddress(e.target.value)}}/>
+        <input type='text' placeholder='Enter address...' value={address} onChange={(e)=>{setAddress(e.target.value)}}/>
         <label htmlFor='creditCard'>Credit-card:</label>
-        <input type='text' placeholder='Enter credit card...' onChange={(e)=>{setCredit(e.target.value)}}/>
+        <input type='text' placeholder='Enter credit card...' value={credit} onChange={(e)=>{setCredit(e.target.value)}}/>
         <button>Checkout</button>
       </form>
     </div>
