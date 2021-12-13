815ac42724a856fdafbc63a2aa7a49ea2d33df0e
typeluis <typeluis@gmail.com>
2021-12-13 13:08:53 -0500

    Loading screen

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './LoadingScreen.css'
+
+const LoadingScreen = () => {
+    return (
+        <div className='loading-screen'>
+
+            <div className='loading'>
+
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default LoadingScreen
