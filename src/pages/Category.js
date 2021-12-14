56f790b68bd42c8b4336081e4209524c02d7dd2b
jennielai <jennie.t.lai001@gmail.com>
2021-12-14 01:48:40 -0500

    added carousel

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -4,6 +4,13 @@ import env from 'react-dotenv'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import Logo from './images/logo.png'
+import Logo2 from './images/logo2.png'
+import Logo3 from './images/logo3.png'
+import Logo4 from './images/logo4.png'
+
 const Category = () => {
 
     // const [items, setItems] = useState({})
@@ -21,23 +28,40 @@ const Category = () => {
     // }
 
     // useEffect(()=>{loadItems()}, [])
-
+    const responsive = {
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+      },
+    }
     return (
-        <div>
-            <div>Categories</div>
-            <div>
-                <Link to='/category/bakery'>Bakery</Link>
-            </div>
-            <div>
-                <Link to='/category/books'>Books</Link>
-            </div>
-            <div>
-                <Link to='/category/electronics'>Electronics</Link>
-            </div>
-            <div>
-                <Link to='/category/plants'>Plants</Link>
-            </div>
-        </div>
+      <Carousel responsive={responsive}>
+      <div>
+        <Link to="/category/bakery"> <img src={Logo} alt={"logo"}/> </Link>
+      </div>
+      <div>
+      <Link to="/category/books"> <img src={Logo4} alt={"logo"}/> </Link>
+      </div>
+      <div>
+        <Link to="/category/electronics"> <img src={Logo2} alt={"logo"}/> </Link>
+      </div>
+      <div>
+        <Link to="/category/plants"> <img src={Logo3} alt={"logo"}/> </Link>
+      </div>
+    </Carousel>
     )
 }
 
