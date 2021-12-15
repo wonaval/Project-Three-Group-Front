169c077244dd9b021668dd1ee2a24b97ee0dd0de
jennielai <jennie.t.lai001@gmail.com>
2021-12-15 00:26:09 -0500

    wip:added grid & icons

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -5,28 +5,24 @@ import { UserContext } from '../context/UserContext'
 import env from 'react-dotenv'
 import axios from 'axios'
 
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+
+import addToCart from './images/icon.png'
+
 const AllProducts = (props) => {
     const { userState } = useContext(UserContext)
     const [ user, setUser ] = userState
-
-
     const {name} = useParams()
-
     const [products, setProducts] = useState([])
-
     const loadProducts = async () => {
         const response = await axios.get(`${env.BACKEND_URL}/item`)
-        
-        console.log(response)
-
         setProducts(response.data.items)
-
         
-        console.log(name)
-
-        // setTimeout(()=>{console.log(products)},5000)
     }
-
     const addToCartClick = async (itemId) => {
         const response = await axios.post(`${env.BACKEND_URL}/cart`, {id : itemId}, {
             headers: {
@@ -39,31 +35,41 @@ const AllProducts = (props) => {
     useEffect(()=>{
         loadProducts()
     }, [])
-    
+
     return (
-        <div>
-            <div>
-                <Link to='/category'>Back to Categories</Link>
-            </div>
-            <div>
-                {products.map((item, i) => {
-                    return(
-                        <div key={item.id}>
-                            {item.category === name ?
-                                <>
-                                    <h1><Link to={`/item/${item.id}`}>{item.name}</Link></h1>
-                                    <img src={item.image} alt={`Image of ${item.name}`} />
-                                    <button onClick={()=>{addToCartClick(item.id)}}>Add to cart</button>
-                                </>
-                            :
-                                null
-                            }
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+        <ImageList sx={{ width: 1050, height: 450 }}>
+        <ImageListItem key="Subheader" cols={7}>
+        <ListSubheader component="div">{name}</ListSubheader>
+        </ImageListItem>
+        {products.map((item, i) => (
+            <ImageListItem key="item.id">
+                <img 
+                src={`${item.image}?
+                w=248&fit=crop&auto=format`}
+                srcSet={`${item.image}?
+                w=248&fit=crop&auto=format&dpr=6 6x`}
+                alt={item.title}
+                loading="lazy"
+                />
+            <ImageListItemBar
+                title={item.name}
+                subtitle={item.description}
+                actionIcon={
+            <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${item.title}`}
+  >
+            <button> <img src={addToCart} onClick={()=>{addToCartClick(item.id)}} /> 
+            </button>
+            </IconButton>
+  
+}
+/>
+            
+            </ImageListItem>
+        ))}
+        </ImageList>
+  )
 }
 
 export default AllProducts
