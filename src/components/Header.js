bee7bbe74b28862e9be3479d7efbf9ce2fbf0ffe
jennielai <jennie.t.lai001@gmail.com>
2021-12-15 00:26:35 -0500

    wip:added the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,68 +2,47 @@ import React, { useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
 
-const Header = (props) => {
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+const Header = () => {
     const { userState, cartState } = useContext(UserContext)
     const [ user, setUser ] = userState
     const [ cart, setCart ] = cartState
-
-
+    
     return (
-        <div>
-            <ul className='nav-bar'>
-                <>
-                    <li>
-                        <Link
-                            to="/category">
-                            Category
-                        </Link>
-                    </li>
-                { user.id ?
-                    <>
-                        <li>
-                            <Link
-                                to="/cart">
-                                My Cart
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link
-                                to="/orders">
-                                My Orders
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link to='/'
-                            onClick={()=>{
-                                setUser({})
-                                setCart([])
-                                localStorage.removeItem('userId')
-                            }}
-                            >Logout</Link>
-                        </li>
-                    </>
-                :
-                    <>
-                        <li>
-                            <Link
-                                to="/signup">
-                                Sign Up
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link
-                                to="/login">
-                                Login
-                            </Link>
-                        </li>
-                    </>
-                }
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+            </IconButton>
+            { user.id ? 
+            <>
+            <Button href="/category" color="inherit"> Category </Button>
+            <Button href="/cart" color="inherit"> My Cart </Button>
+            <Button href="/orders" color="inherit"> My Orders </Button>
+            <Button href="/" color= "inherit" onClick={() => { setUser({}); setCart([]); 
+            localStorage.removeItem('userId') }} >Logout </Button> 
             </>
-            </ul>
-        </div>
+            :
+            <>
+            <Button href="/signup" color="inherit"> Sign Up </Button>
+            <Button href="/login" color="inherit"> Login </Button>
+            </>
+            }
+            
+          </Toolbar>
+        </AppBar>
+      </Box>
     )
 }
 
