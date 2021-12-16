c51e7cf1ebb081923b7c2680ca52e70add14d611
wonaval <wonaval@gmail.com>
2021-12-16 17:45:56 -0500

    Fixed My Orders

--- src/pages/MyOrders.js
+++ src/pages/MyOrders.js
@@ -13,58 +13,66 @@ const MyOrders = (props) => {
   const [ cart, setCart ] = useState([])
   const [ cartInfo, setCartInfo ] = useState([])
 
-  // Gets cart from backend
-  const getCart = async () => {
-    try {
-      const response = await axios.get(`${env.BACKEND_URL}/cart`, {
-      headers: { Authorization: localStorage.getItem('userId')}
-    })
-    await setCart(response.data.items)
-    } catch (error) {
-      console.log(error)
-    }
+    // Get cart from backend
+    const getCart = () => {
+      const userId = localStorage.getItem('userId')
+      try {
+          // GET cart from backend
+          axios.get(`${env.BACKEND_URL}/cart`,{
+              headers: { Authorization: userId }
+          }).then((cartResponse)=>{setCart([...cartResponse.data.items])})
+
+      } catch (error) {
+          console.log(error.message)
+      }
   }
 
-  // Converts cartState context into productInfo to be displayed
+  // Converts cart context into productInfo to be displayed
   const itemInfo = async () => {
-    try {
-        const infoList = cart.map((item)=>{
+      // Filters list so only checked out items are left
+      const checkedList = await cart.filter((item)=>{return(item.checkedOut === true)})
+      console.log('checked', checkedList)
+
+      const infoList = await checkedList.map((item)=>{
           return (props.products.find((product)=>{ return (product.id === item.itemId) }))
-        })
-        await setCartInfo(infoList)
-    } catch (error) {
-        console.log(error.message)
-    }
+      })
+      await setCartInfo([...infoList])
+      console.log('CART', cartInfo)
   }
 
   // Get unique dates in cart join table
   const getCartDate = async () => {
+    // const List1 = await [...new Set(cart.map(item => item.checkoutDate))]
+    // Referenced: https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript?rq=1
 
+    // Alternative function
     // referenced: https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript?rq=1
-    // const List = await cart.map((item) => {
-    //   console.log('Item', item)
-    //   return item.checkoutDate
-    // }).filter((value, index, self)=>(
-    //   self.indexOf(value) === index
-    // ))
+    console.log(cart)
+    const List = await cart.map((item) => {
+      console.log('Item', item)
+      return item.checkoutDate
+    }).filter((value, index, self)=>(
+      self.indexOf(value) === index
+    ))
 
-
-
-    // referenced: https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript?rq=1
-    const List1 = await [...new Set(cart.map(item => item.checkoutDate))]
-
-    await console.log('DateList', List1)
+    await console.log('DateList', List)
     await console.log('Cart', cart)
-    const sorted = await List1.sort()
+    const sorted = await List.sort()
     await setUniqueDate(sorted)
 
   }
 
   useEffect(()=>{
     getCart();
-    itemInfo();
-    getCartDate();
   }, [])
+
+  useEffect(()=>{
+    itemInfo()
+  },[cart])
+
+  useEffect(()=>{
+    getCartDate();
+  },[cartInfo])
 
   return (
     <div>
